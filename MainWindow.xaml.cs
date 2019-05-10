a24d92e5d5478b3705e4f128d66c62bc1addb0ec
Marvin <lionking89@ufl.edu>
2019-05-10 14:29:08 -0400

    Added the DrawX, DrawY, and DrawBlank functions to the xaml.cs.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,28 +20,35 @@ namespace tic_tac_toe
     /// </summary>
     public partial class MainWindow : Window
     {
-        Image[] imageCells = new Image[9];  
+        Image[,] imageCells = new Image[3,3];  
         public MainWindow()
         {
             InitializeComponent();
 
-            imageCells[0] = cell_0_0;
-            imageCells[1] = cell_0_1;
-            imageCells[2] = cell_0_2;
-            imageCells[3] = cell_1_0;
-            imageCells[4] = cell_1_1;
-            imageCells[5] = cell_1_2;
-            imageCells[6] = cell_2_0;
-            imageCells[7] = cell_2_1;
-            imageCells[8] = cell_2_2;
+            imageCells[0,0] = cell_0_0;
+            imageCells[0,1] = cell_0_1;
+            imageCells[0,2] = cell_0_2;
+            imageCells[1,0] = cell_1_0;
+            imageCells[1,1] = cell_1_1;
+            imageCells[1,2] = cell_1_2;
+            imageCells[2,0] = cell_2_0;
+            imageCells[2,1] = cell_2_1;
+            imageCells[2,2] = cell_2_2;
 
+            DrawX(0, 0);
+            DrawO(0, 1);
+            DrawBlank(1, 0);
+        }
+
+        // This function draws x.png into the image cell cell_i_j
+        private void DrawX(int i, int j)
+        {
             System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
 
             string[] s = executingAssembly.GetManifestResourceNames();
 
-
-            // Getting bytes for image x.png from the executingAssembly. 
-            System.IO.Stream stream =  executingAssembly.GetManifestResourceStream("tic_tac_toe.images.x.png");
+            // Getting bytes for image "x.png" from the executingAssembly. 
+            System.IO.Stream stream = executingAssembly.GetManifestResourceStream("tic_tac_toe.images.x.png");
             byte[] b = new byte[stream.Length];
             stream.Read(b, 0, (int)stream.Length);
             // b now contains the raw bytes for the image x.png. 
@@ -59,9 +66,61 @@ namespace tic_tac_toe
                 img.EndInit();
             }
             img.Freeze();
+            imageCells[i, j].Source = img;
+        }
 
-           
-            cell_0_0.Source = img;
+        private void DrawO(int i, int j)
+        {
+            System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            string[] s = executingAssembly.GetManifestResourceNames();
+
+            // Getting bytes for image "o.png" from the executingAssembly.
+            System.IO.Stream stream = executingAssembly.GetManifestResourceStream("tic_tac_toe.images.o.png");
+            byte[] b = new byte[stream.Length];
+            stream.Read(b, 0, (int)stream.Length);
+
+            var img = new BitmapImage();
+
+            using (var mem = new System.IO.MemoryStream(b))
+            {
+                mem.Position = 0;
+                img.BeginInit();
+                img.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                img.CacheOption = BitmapCacheOption.OnLoad;
+                img.UriSource = null;
+                img.StreamSource = mem;
+                img.EndInit();
+            }
+            img.Freeze();
+            imageCells[i, j].Source = img;
+        }
+
+        private void DrawBlank(int i, int j)
+        {
+            System.Reflection.Assembly executingAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            string[] s = executingAssembly.GetManifestResourceNames();
+
+            // Getting bytes for image "blank.png" from the executingAssembly.
+            System.IO.Stream stream = executingAssembly.GetManifestResourceStream("tic_tac_toe.images.blank.png");
+            byte[] b = new byte[stream.Length];
+            stream.Read(b, 0, (int)stream.Length);
+
+            var img = new BitmapImage();
+
+            using (var mem = new System.IO.MemoryStream(b))
+            {
+                mem.Position = 0;
+                img.BeginInit();
+                img.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                img.CacheOption = BitmapCacheOption.OnLoad;
+                img.UriSource = null;
+                img.StreamSource = mem;
+                img.EndInit();
+            }
+            img.Freeze();
+            imageCells[i, j].Source = img;
         }
     }
 }
